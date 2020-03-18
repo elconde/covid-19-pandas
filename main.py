e9eec28f200c86ec98384ef2497f759f5808aa4e
Robert Conde <rconde@broadwaytechnology.com>
2020-03-18 10:04:06 -0400

    Messing around with indexes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Load libraries
-from pandas import read_csv
+import pandas
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
@@ -14,14 +14,25 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+import os
+
+PROJECT_DIR = os.path.dirname(__file__)
 
 
 def main():
     """Load dataset"""
-    url = r"C:\Users\rconde\Projects\covid-19\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_19-covid-Confirmed.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width',
-             'class']
-    dataset = read_csv(url, names=names)
+    csv_file_name = os.path.join(
+        PROJECT_DIR, 'COVID-19', 'csse_covid_19_data',
+        'csse_covid_19_time_series', 'time_series_19-covid-Confirmed.csv'
+    )
+    dataset = pandas.read_csv(csv_file_name).transpose()
+    dataframe = pandas.DataFrame(dataset)
+    dataframe.drop(['Lat', 'Long'],axis='index', inplace=True)
+    dataframe.columns = dataframe.iloc[1]
+    dataframe.drop(dataframe.index[1], inplace=True)
+    columns = [column for column in dataframe.columns if column == 'US']
+    dataframe = dataframe[columns]
+    print(dataframe)
 
 
 if __name__ == '__main__':
